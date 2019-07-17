102d43e0ee3b81cc24cbf21ef3a420d60157bbec
EC2 Default User <ec2-user@ip-172-31-23-0.ap-southeast-1.compute.internal>
2019-07-17 21:50:31 +0000

    databaseSeeder.phpを更新

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+       $this->call([
+           UserTableSeeder::class,
+           CategoryTableSeeder::class,
+           BookTableSeeder::class,
+           UserInfoTableSeeder::class,
+           OrderTableSeeder::class,
+       ]);
     }
 }
